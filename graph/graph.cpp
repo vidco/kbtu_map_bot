19cdb3069b82cdbebdf3bd000bd1997d166c7659
thesafatem <thesafatem@gmail.com>
2020-05-08 00:34:12 +0600

    add path description, change floors in .csv

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -5,18 +5,26 @@
 #include <queue>
 #include <sstream>
 #include <fstream>
+#include <string>
 
 namespace py = pybind11;
+
+enum Direction {
+	UP = 1,
+	RIGHT = 2,
+	DOWN = 3,
+	LEFT = 4
+};
 
 struct Node {
 	int id;
 	std::string location;
 	int x;
 	int y;
-	std::string floor;
+	int floor;
 	std::vector<int> adjacents;
 
-	Node(int id, std::string location, int x, int y, std::string floor, std::vector<int> adjacents) {
+	Node(int id, std::string location, int x, int y, int floor, std::vector<int> adjacents) {
 		this->id = id;
 		this->location = location;
 		this->x = x;
@@ -37,7 +45,7 @@ struct Node {
 		return std::make_pair(this->x, this->y);
 	}
 
-	std::string getFloor() {
+	int getFloor() {
 		return this->floor;
 	}
 
@@ -68,7 +76,7 @@ struct Graph {
 			std::string location = row[1];
 			int x = stoi(row[2]);
 			int y = stoi(row[3]);
-			std::string floor = row[4];
+			int floor = stoi(row[4]);
 			std::stringstream adj(row[5]);
 			std::string nei;
 			while (getline(adj, nei, '-')) {
@@ -126,10 +134,75 @@ struct Graph {
 		return path;
 	}
 
-	std::map<std::string, std::vector<std::vector<std::pair<int, int> > > > getPathOnFloor(std::vector<int> path) {
-		std::map<std::string, std::vector<std::vector<std::pair<int, int> > > > pathOnFloor;
+	Direction getDirection(std::pair<int, int> from, std::pair<int, int> to) {
+		int xd = to.first - from.first;
+		int yd = to.second - from.second;
+		if (!xd) {
+			if (yd > 0) {
+				return Direction.DOWN;
+			} else {
+				return Direction.UP;
+			}
+		} else {
+			if (xd > 0) {
+				return Direction.RIGHT;
+			} else {
+				return Direction.LEFT;
+			}
+		}
+	}
+
+	std::string getCross(Direction first, Direction second) {
+		if (first == second) {
+			return "straight";
+		} else if ((second - first + 4) % 4 == 1) {
+			return "right";
+		} else {
+			return "left";
+		}
+	} 
+
+	std::string pathDescription(std::vector<int> path) {
+		std::string textPath = "";
+		for (int i = 0; i < int(path.size()); i++) {
+			string location = getNode(path[i]).getLocation();
+			if (location == "cross") {
+				std::pair<int, int> prevCoords = getNode(path[i - 1]).getCoordinates();
+				std::pair<int, int> curCoords = getNode(path[i]).getCoordinates();
+				std::pair<int, int> nextCoords = getNode(path[i + 1]).getCoordinates();
+				Direction first = getDirection(prevCoords, curCoords);
+				Direction second = getDirection(curCoords, nextCoords);
+				std::string cross = getCross(first, second);
+				textPath += cross;
+			} else if (location == "ladder") {
+				int prevFloor = getNode(path[i]).getFloor();
+				while (location == "ladder") {
+					i++;
+					location = getNode(path[i]).getLocation();
+				}
+				i--;
+				int nextFloor = getNode(path[i]).getFloor();
+				if (nextFloor > prevFloor) {
+					textPath += "go upstairs to the ";
+				} else {
+					textPath += "go downstairs to the ";
+				}
+				textPath += to_string(nextFloor);
+				textPath += " floor";
+			} else {
+				textPath += location;
+			}
+
+			if (i != int(path.size())) {
+				textPath += " -> ";
+			}
+		}
+	}
+
+	std::map<int, std::vector<std::vector<std::pair<int, int> > > > getPathOnFloor(std::vector<int> path) {
+		std::map<int, std::vector<std::vector<std::pair<int, int> > > > pathOnFloor;
 		std::vector<std::pair<int, int> > currentPath;
-		std::string currentFloor;
+		int currentFloor;
 		currentPath.push_back(getNode(path[0]).getCoordinates());
 		currentFloor = getNode(path[0]).getFloor();
 		for (int i = 1; i < int(path.size()); i++) {
@@ -151,7 +224,7 @@ struct Graph {
 
 PYBIND11_MODULE(graph, m) {
     py::class_<Node>(m, "Node")
-    	.def(py::init<int, std::string, int, int, std::string, std::vector<int> >())
+    	.def(py::init<int, std::string, int, int, int, std::vector<int> >())
     	.def("get_id", &Node::getId)
     	.def("get_location", &Node::getLocation)
     	.def("get_coordinates", &Node::getCoordinates)
@@ -163,5 +236,15 @@ PYBIND11_MODULE(graph, m) {
     	.def("check_by_name", &Graph::checkByName)
     	.def("get_node", &Graph::getNode)
     	.def("get_min_dist", &Graph::getMinDist)
-    	.def("get_path_on_floor", &Graph::getPathOnFloor);
+    	.def("get_path_on_floor", &Graph::getPathOnFloor)
+    	.def("get_direction", &Graph::getDirection)
+    	.def("get_cross", &Graph::getCross)
+    	.def("path_description", &Graph::pathDescription);
+
+    py::enum_<Direction>(m, "Direction")
+    	.value("UP", Direction::UP)
+    	.value("DOWN", Direction::DOWN)
+    	.value("LEFT", Direction::LEFT)
+    	.value("RIGHT", Direction::RIGHT)
+    	.export_values();
 }
