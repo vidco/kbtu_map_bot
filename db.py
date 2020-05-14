6b652314f0424121e2020b477ecc7042836266ff
mebr0 <aza17e@gmail.com>
2020-05-14 21:39:56 +0600

    Add with connection structures

--- db.py
+++ db.py
@@ -15,102 +15,87 @@ class Database:
 		LOG.info('Connected to database')
 
 		if not os.path.exists(path):
-			connection = sqlite3.connect(path)
-
 			query = '''CREATE TABLE IF NOT EXISTS users (
 							telegram_id integer PRIMARY KEY,
-		 					language text DEFAULT "kz",
-		 					level int DEFAULT 0
+							language text DEFAULT "kz",
+							level int DEFAULT 0
 						);'''
-			cursor = connection.cursor()
-			cursor.execute(query)
+
+			with sqlite3.connect(self.path) as connection:
+
+				cursor = connection.cursor()
+				cursor.execute(query)
+
+				connection.commit()
 
 			LOG.info('Table users created')
-			connection.close()
 
 	def create_user(self, user):
-		connection = sqlite3.connect(self.path)
-
 		query = 'INSERT INTO users(telegram_id) VALUES(?)'
-		cursor = connection.cursor()
-		cursor.execute(query, (user,))
 
-		connection.commit()
-		connection.close()
+		with sqlite3.connect(self.path) as connection:
+			cursor = connection.cursor()
+			cursor.execute(query, (user,))
 
-		LOG.info(f'User with telegram id {cursor.lastrowid} registered')
+			connection.commit()
+
+			LOG.info(f'User with telegram id {cursor.lastrowid} registered')
+
 		return cursor.lastrowid
 
 	def select_language_by_telegram_id(self, telegram_id):
-		connection = sqlite3.connect(self.path)
-
 		query = 'SELECT language FROM users WHERE telegram_id=?'
-		cursor = connection.cursor()
-		cursor.execute(query, (telegram_id,))
 
-		language = cursor.fetchone()
+		with sqlite3.connect(self.path) as connection:
+			cursor = connection.cursor()
+			cursor.execute(query, (telegram_id,))
 
-		connection.close()
+			language = cursor.fetchone()
 
-		return language[0]
+		if language:
+			return language[0]
+		return None
 
 	def select_level_by_telegram_id(self, telegram_id):
-		connection = sqlite3.connect(self.path)
-
 		query = 'SELECT level FROM users WHERE telegram_id=?'
-		cursor = connection.cursor()
-		cursor.execute(query, (telegram_id,))
 
-		level = cursor.fetchone()
+		with sqlite3.connect(self.path) as connection:
+			cursor = connection.cursor()
+			cursor.execute(query, (telegram_id,))
 
-		connection.close()
+			level = cursor.fetchone()
 
 		return level[0]
 
 	def update_user_language(self, user):
-		connection = sqlite3.connect(self.path)
-
 		query = '''UPDATE users
 					SET language = ?
 					WHERE telegram_id = ?'''
-		cursor = connection.cursor()
-		cursor.execute(query, user)
 
-		connection.commit()
-		connection.close()
+		with sqlite3.connect(self.path) as connection:
+			cursor = connection.cursor()
+			cursor.execute(query, user)
+
+			connection.commit()
 
 	def update_user_level(self, user):
-		connection = sqlite3.connect(self.path)
-
 		query = '''UPDATE users
 					SET level = ?
 					WHERE telegram_id = ?'''
-		cursor = connection.cursor()
-		cursor.execute(query, user)
 
-		connection.commit()
-		connection.close()
+		with sqlite3.connect(self.path) as connection:
+			cursor = connection.cursor()
+			cursor.execute(query, user)
+
+			connection.commit()
 
 	def user_exists(self, telegram_id):
-		connection = sqlite3.connect(self.path)
+		query = 'SELECT count(*) FROM users WHERE telegram_id=?'
 
-		query = 'SELECT COUNT(*) FROM users WHERE telegram_id=?'
-		cursor = connection.cursor()
-		cursor.execute(query, (telegram_id,))
+		with sqlite3.connect(self.path) as connection:
+			cursor = connection.cursor()
+			cursor.execute(query, (telegram_id,))
 
-		exists = cursor.fetchone()
-		connection.close()
+			exists = cursor.fetchone()
 
 		return bool(exists[0])
-
-	def delete_user(self, user):
-		connection = sqlite3.connect(self.path)
-
-		query = 'DELETE FROM users WHERE telegram_id=?'
-		cur = connection.cursor()
-		cur.execute(query, user)
-
-		connection.commit()
-		connection.close()
-
-		LOG.info(f'User with telegram id {cursor.lastrowid} unregistered')
